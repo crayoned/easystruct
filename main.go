235e2ee9ec846b72146e3f981010e77171886e34
Andrey Tsvetkov <tsvetkov.msk@gmail.com>
2023-02-06 09:47:44 +0300

    multiply files

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	_ "embed"
+	"flag"
 	"fmt"
 	"go/ast"
 	"go/parser"
@@ -17,108 +18,118 @@ import (
 var tmpl string
 
 func main() {
-	inputFile := os.Args[2]
-	outputFile := strings.Replace(inputFile, ".go", "_easystruct.go", 1)
 
-	pf, err := parser.ParseFile(token.NewFileSet(), inputFile, nil, 0)
-	if err != nil {
-		panic(err)
-	}
+	flag.Parse()
 
-	data := fileData{
-		Imports: map[string]struct{}{
-			"net/http": {},
-		},
-	}
+	for i := 0; i < flag.NArg(); i++ {
 
-	ast.Inspect(pf, func(node ast.Node) bool {
-		p, ok := node.(*ast.File)
-		if ok {
-			data.Package = p.Name.Name
-			return true
+		inputFile := flag.Arg(i)
+		outputFile := strings.Replace(inputFile, ".go", "_easystruct.go", 1)
+
+		pf, err := parser.ParseFile(token.NewFileSet(), inputFile, nil, 0)
+		if err != nil {
+			panic(err)
 		}
 
-		ts, ok := node.(*ast.TypeSpec)
-		if !ok || ts.Type == nil {
-			return true
+		data := fileData{
+			Imports: map[string]struct{}{
+				"net/http": {},
+			},
 		}
 
-		st, ok := ts.Type.(*ast.StructType)
-		if !ok {
-			return true
-		}
+		ast.Inspect(pf, func(node ast.Node) bool {
+			p, ok := node.(*ast.File)
+			if ok {
+				data.Package = p.Name.Name
+				return true
+			}
 
-		fields := make([]fieldData, 0, len(st.Fields.List))
-	FIELDS:
-		for _, item := range st.Fields.List {
-			source, name, ok := matchTagValue("es", item)
+			ts, ok := node.(*ast.TypeSpec)
+			if !ok || ts.Type == nil {
+				return true
+			}
+
+			st, ok := ts.Type.(*ast.StructType)
 			if !ok {
-				continue
+				return true
 			}
 
-			var field fieldData
+			fields := make([]fieldData, 0, len(st.Fields.List))
+		FIELDS:
+			for _, item := range st.Fields.List {
+				source, name, ok := matchTagValue("es", item)
+				if !ok {
+					continue
+				}
 
-			switch source {
-			case "query":
-				field.Source = fmt.Sprintf("r.URL.Query().Get(%q)", name)
-			case "header":
-				field.Source = fmt.Sprintf("r.Header.Get(%q)", name)
-			case "formData":
-				field.Source = fmt.Sprintf("r.FormValue(%q)", name)
-			default:
-				continue FIELDS
-			}
+				var field fieldData
 
-			switch ft := item.Type.(type) {
-			default:
-				continue FIELDS
-			case *ast.Ident:
-				field.Type = ft.Name
-			}
+				switch source {
+				case "query":
+					field.Source = fmt.Sprintf("r.URL.Query().Get(%q)", name)
+				case "header":
+					field.Source = fmt.Sprintf("r.Header.Get(%q)", name)
+				case "formData":
+					field.Source = fmt.Sprintf("r.FormValue(%q)", name)
+				default:
+					continue FIELDS
+				}
 
-			switch field.Type {
-			case "string":
-				field.Kind = "varchar"
-			case "int", "int8", "int16", "int32", "int64", "uint", "uint8", "uint16", "uint32", "uint64":
-				field.Kind = "integer"
-				data.Imports["strconv"] = struct{}{}
-			case "float64", "float32":
-				field.Kind = "double"
-				data.Imports["strconv"] = struct{}{}
-			case "bool":
-				field.Kind = "boolean"
-				data.Imports["strconv"] = struct{}{}
-			default:
-				continue FIELDS
-			}
+				switch ft := item.Type.(type) {
+				default:
+					continue FIELDS
+				case *ast.Ident:
+					field.Type = ft.Name
+				}
 
-			for _, name := range item.Names {
-				if ok := name.IsExported(); ok {
-					field.Name = name.Name
+				switch field.Type {
+				case "string":
+					field.Kind = "varchar"
+				case "int", "int8", "int16", "int32", "int64", "uint", "uint8", "uint16", "uint32", "uint64":
+					field.Kind = "integer"
+					data.Imports["strconv"] = struct{}{}
+				case "float64", "float32":
+					field.Kind = "double"
+					data.Imports["strconv"] = struct{}{}
+				case "bool":
+					field.Kind = "boolean"
+					data.Imports["strconv"] = struct{}{}
+				default:
+					continue FIELDS
+				}
+
+				for _, name := range item.Names {
+					if ok := name.IsExported(); ok {
+						field.Name = name.Name
+					}
+				}
+				if field.Name != "" {
+					fields = append(fields, field)
 				}
 			}
-			if field.Name != "" {
-				fields = append(fields, field)
-			}
-		}
 
-		data.Structs = append(data.Structs, structData{
-			Name:     ts.Name.Name,
-			Receiver: onlyUppers(ts.Name.Name),
-			Fields:   fields,
+			data.Structs = append(data.Structs, structData{
+				Name:     ts.Name.Name,
+				Receiver: onlyUppers(ts.Name.Name),
+				Fields:   fields,
+			})
+
+			return true
 		})
 
-		return true
-	})
+		if len(data.Structs) == 0 {
+			continue
+		}
 
-	target, err := os.Create(outputFile)
-	if err != nil {
-		panic(err)
-	}
-	defer target.Close()
+		target, err := os.Create(outputFile)
+		if err != nil {
+			panic(err)
+		}
+		defer target.Close()
 
-	if err := template.Must(template.New("").Parse(tmpl)).Execute(target, data); err != nil {
-		panic(err)
+		if err := template.Must(template.New("").Parse(tmpl)).Execute(target, data); err != nil {
+			panic(err)
+		}
 	}
 }
 
